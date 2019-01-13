f3059687463bd89875344a6d49d8096f837ee7a3
jestonblu <jestonblu@gmail.com>
2019-01-13 00:01:56 -0600

    update

--- R/RobinHood.R
+++ R/RobinHood.R
@@ -1,6 +1,6 @@
 #' Connect to your RobinHood Account
 #'
-#' This function returns an object of S3 class RobinHood and establishes a 
+#' This function returns an object of S3 class RobinHood and establishes a
 #' connection to a RobinHood account.
 #'
 #' @param username user name or email
@@ -11,62 +11,62 @@
 #' rh <- RobinHood(username = 'your username', password = 'your password')
 #' getPositions(rh)
 RobinHood <- function(username, password) {
-  
+
   # Storage for api data
   RH = list(
     # APIs
     api.grantType = "password",
     api.clientID = "c82SH0WZOsabOXGP2sxqcj34FxkvfnWRZBKlBjFS"
   )
-  
+
   ##############################################################################
-  
-  library(curl)
-  library(jsonlite)
-  library(magrittr)
+
+  requireNamespace(curl)
+  requireNamespace(jsonlite)
+  requireNamespace(magrittr)
 
   detail <- paste("grant_type=", RH$api.grantType,
                   "&client_id=", RH$api.clientID,
                   "&username=", username,
                   "&password=", password, sep = "")
-  
+
   # Log in, get access token
   auth <- new_handle() %>%
     handle_setopt(copypostfields = detail) %>%
     handle_setheaders("Accept" = "application/json") %>%
-    curl_fetch_memory(url = getURL("token")) %$% content %>% 
-    rawToChar %>% 
+    curl_fetch_memory(url = getURL("token")) %$% content %>%
+    rawToChar %>%
     fromJSON
-  
-  
+
+
   access_token <- auth$access_token
   refresh_token <- auth$refresh_token
-  
+
   # Get account id
   user <- new_handle() %>%
     handle_setheaders("Accept" = "application/json") %>%
     handle_setheaders("Authorization" = paste("Bearer", access_token)) %>%
-    curl_fetch_memory(url = getURL("accounts")) %$% content %>% 
-    rawToChar %>% 
+    curl_fetch_memory(url = getURL("accounts")) %$% content %>%
+    rawToChar %>%
     fromJSON
 
   url_positions <- user$results$positions
-  
-  
-  # Return object  
+
+
+  # Return object
    RH = c(RH,
-         
+
          # User tokens and ids
          tokens = list(
            access_token = access_token,
            refresh_token = refresh_token),
-         
+
          # User url
          user.url = list(
            positions = url_positions
          )
    )
-   
+
    class(RH) <- "RobinHood"
 
    return(RH)
