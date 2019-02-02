98b062f858f0ff48c682b8b7a64de050f3269b35
Jeston <JestonBlu@gmail.com>
2019-02-02 08:15:50 -0600

    updated order_history to include symbol, updated lintr

--- R/get_order_history.R
+++ R/get_order_history.R
@@ -11,14 +11,29 @@ get_order_history <- function(RH) {
 
   # Get Order History
   order_history <- api_orders(RH, action = "history")
-
-  order_status <- fromJSON(rawToChar(order_history$content))
-  order_status <- order_status$results
-
-  order_status <- order_status[, c("updated_at", "time_in_force", "fees", "state",
-                                   "trigger", "type", "price", "side", "quantity")]
-
-  order_status$updated_at <- ymd_hms(order_status$updated_at)
-
-  return(order_status)
+  order_history <- fromJSON(rawToChar(order_history$content))
+  order_history <- order_history$results
+  
+  # Get symbol to attach to output
+  symbol = as.character()
+  
+  for (i in order_history$instrument) {
+    x <- api_instruments(RH, i)
+    x <- x$symbol
+    symbol <- c(symbol, x)
+  }
+  
+  # Combine symbol with order history
+  order_history$symbol <- symbol
+  order_history <- order_history[, c("updated_at", "symbol", "side", "price", "quantity", "fees", "state", 
+                                   "average_price", "type", "trigger", "time_in_force")]
+  
+  # Format timestamp
+  order_history$updated_at <- ymd_hms(order_history$updated_at)
+  order_history$fees = as.numeric(order_history$fees)
+  order_history$price <- as.numeric(order_history$price)
+  order_history$average_price <- as.numeric(order_history$average_price)
+  order_history$quantity <- as.numeric(order_history$quantity)
+  
+  return(order_history)
 }
