9f7f6e446912760838877c999f7a4a87edb3eba1
jestonblu <jestonblu@gmail.com>
2019-03-29 15:30:44 -0500

    added get_historicals

---
+++ R/api_historicals.R
@@ -0,0 +1,24 @@
+#' RobinHood API: Historicals
+#'
+#' Backend function called by get_historicals(). Returns a data frame of historical price data.
+#'
+#' @param RH object of class RobinHood
+#' @param historicals_url (string) api url
+#' @param body (string) api body
+#' @export
+#' @import curl jsonlite magrittr lubridate
+api_historicals <- function(RH, historicals_url, body) {
+
+  historicals_url = paste(historicals_url, "?", body, sep = "")
+
+  historicals <- new_handle() %>%
+    handle_setheaders("Accept" = "application/json") %>%
+    handle_setheaders("Authorization" = paste("Bearer", RH$tokens.access_token)) %>%
+    curl_fetch_memory(url = historicals_url)
+
+  historicals <- fromJSON(rawToChar(historicals$content))
+  historicals <- data.frame(historicals$results$historicals)
+
+  historicals$begins_at = lubridate::ymd_hms(historicals$begins_at)
+  return(historicals)
+  }
---
+++ R/get_historicals.R
@@ -0,0 +1,31 @@
+#' Get historical price history from RobinHood
+#'
+#' Returns a data frame of historical price history for a given symbol.
+#'
+#' @param RH object of class RobinHood
+#' @param symbol (string) Stock symbol to query, single symbol only
+#' @param interval (string) Interval of time to aggregate to (examples: hour, day, week, month)
+#' @param span (string) Period of time you are interested in (examples: day, week, month, year)
+#' @import curl jsonlite magrittr
+#' @export
+#' @examples
+#' \dontrun{
+#' # Login in to your RobinHood account
+#' RH <- RobinHood("username", "password")
+#'
+#' get_historicals (RH = RH, symbol = "CAT", interval = "day", span = "month")
+#'
+#'}
+get_historicals <- function(RH, symbol, interval, span) {
+
+  historicals_url = api_endpoints("historicals")
+
+  body <- paste("symbols=", symbol,
+                "&interval=", interval,
+                "&span=", span,
+                sep = "")
+
+  historicals <- api_historicals(RH, historicals_url, body)
+
+  return(historicals)
+  }
