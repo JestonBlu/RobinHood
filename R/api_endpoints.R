48a7c8c9a0bd8acaef08ff072fdda4183b04a618
JestonBlu <jestonblu@gmail.com>
2019-04-05 22:55:20 -0500

    added api_option_instruments

--- R/api_endpoints.R
+++ R/api_endpoints.R
@@ -7,23 +7,24 @@
 api_endpoints <- function(endpoint) {
 
   api.endpoint <- list(
-    url               = "https://api.robinhood.com/",
-    accounts          = "accounts/",
-    fundamentals      = "fundamentals/?symbols=",
-    historicals       = "quotes/historicals/",
-    markets           = "markets/",
-    options           = "options",
-    option_positions  = "options/positions/",
-    option_history    = "options/orders/",
-    orders            = "orders/",
-    portfolios        = "portfolios/",
-    quotes            = "quotes/?symbols=",
-    tags              = "midlands/tags/tag/",
-    instruments       = "instruments/",
-    token             = "oauth2/token/",
-    revoke_token      = "oauth2/revoke_token/",
-    user              = "user/",
-    watchlist         = "watchlists/"
+    url                = "https://api.robinhood.com/",
+    accounts           = "accounts/",
+    fundamentals       = "fundamentals/?symbols=",
+    historicals        = "quotes/historicals/",
+    markets            = "markets/",
+    options            = "options",
+    option_positions   = "options/positions/",
+    option_history     = "options/orders/",
+    option_instruments = "options/instruments",
+    orders             = "orders/",
+    portfolios         = "portfolios/",
+    quotes             = "quotes/?symbols=",
+    tags               = "midlands/tags/tag/",
+    instruments        = "instruments/",
+    token              = "oauth2/token/",
+    revoke_token       = "oauth2/revoke_token/",
+    user               = "user/",
+    watchlist          = "watchlists/"
   )
 
   x <- which(names(api.endpoint) == endpoint)
---
+++ R/api_option_instruments.R
@@ -0,0 +1,32 @@
+#' RobinHood API: Option Contract Instruments
+#'
+#' @param RH object of class RobinHood
+#' @export
+#' @import curl jsonlite magrittr lubridate
+api_option_instruments <- function(RH, option_instrument_url) {
+
+  option_instrument_url <- option_instrument_url
+
+  option_instruments <- new_handle() %>%
+    handle_setheaders("Accept" = "application/json") %>%
+    handle_setheaders("Authorization" = paste("Bearer", RH$tokens.access_token)) %>%
+    curl_fetch_memory(url = option_instrument_url)
+
+  option_instruments <- fromJSON(rawToChar(option_instruments$content))
+  option_instruments <- data.frame(option_instruments)
+
+  if (option_instruments$type == "call") {
+
+    colnames(option_instruments)[13:15] = c("cutoff_price", "below_tick", "above_tick")
+
+    option_instruments$issue_date <- lubridate::ymd(option_instruments$issue_date)
+    option_instruments$strike_price <- as.numeric(option_instruments$strike_price)
+    option_instruments$expiration_date <- lubridate::ymd(option_instruments$expiration_date)
+    option_instruments$created_at <- lubridate::ymd_hms(option_instruments$created_at)
+    option_instruments$updated_at <- lubridate::ymd_hms(option_instruments$updated_at)
+
+    option_instruments <- option_instruments[, !names(option_instruments) %in% c("url", "chain_id", "id")]
+  }
+
+  return(option_instruments)
+}
