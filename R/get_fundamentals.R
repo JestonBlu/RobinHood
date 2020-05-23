2bbabec006e8cf770bd62f4a3dea676dfcd6d87e
Jeston <jestonblu@gmail.com>
2020-05-23 08:10:39 -0500

    symbols parsed out of `get_fudamentals()` return

--- R/get_fundamentals.R
+++ R/get_fundamentals.R
@@ -14,63 +14,61 @@
 #'}
 get_fundamentals <- function(RH, ticker, include_description = FALSE) {
 
-    # Check if RH is valid
-    check_rh(RH)
+  # Check if RH is valid
+  check_rh(RH)
 
-    # Collapse tickers into a single string
-    ticker <- paste(ticker, collapse = ",")
+  # Collapse tickers into a single string
+  symbol <- ticker
+  ticker <- paste(ticker, collapse = ",")
 
-    # Investment fundamentals call
-    fundamentals <- api_fundamentals(RH, ticker)
+  # Investment fundamentals call
+  fundamentals <- api_fundamentals(RH, ticker)
 
-    # Empty dataframe for results
-    x <- data.frame(
-          open                   = NA,
-          high                   = NA,
-          low                    = NA,
-          volume                 = NA,
-          average_volume_2_weeks = NA,
-          average_volume         = NA,
-          high_52_weeks          = NA,
-          dividend_yield         = NA,
-          float                  = NA,
-          low_52_weeks           = NA,
-          market_cap             = NA,
-          pb_ratio               = NA,
-          pe_ratio               = NA,
-          shares_outstanding     = NA,
-          description            = NA,
-          instrument             = NA,
-          ceo                    = NA,
-          headquarters_city      = NA,
-          headquarters_state     = NA,
-          sector                 = NA,
-          industry               = NA,
-          num_employees          = NA,
-          year_founded           = NA,
-          symbol                 = ticker
-        )
+  # Empty dataframe for results
+  x <- data.frame(
+    open                   = NA,
+    high                   = NA,
+    low                    = NA,
+    volume                 = NA,
+    average_volume_2_weeks = NA,
+    average_volume         = NA,
+    high_52_weeks          = NA,
+    dividend_yield         = NA,
+    float                  = NA,
+    low_52_weeks           = NA,
+    market_cap             = NA,
+    pb_ratio               = NA,
+    pe_ratio               = NA,
+    shares_outstanding     = NA,
+    description            = NA,
+    instrument             = NA,
+    ceo                    = NA,
+    headquarters_city      = NA,
+    headquarters_state     = NA,
+    sector                 = NA,
+    industry               = NA,
+    num_employees          = NA,
+    year_founded           = NA,
+    symbol                 = symbol
+  )
 
-    # If the return all is empty then assign the NA dataframe
-    if (nrow(fundamentals) == 0) {
-         fundamentals <- x
-         fundamentals$symbol <- ticker
-      }
+  # If the return all is empty then assign the NA dataframe
+  if (nrow(fundamentals) == 0) {
+    fundamentals <- x
+    fundamentals$symbol <- symbol
+  }
 
-    # CHECK THIS
-    fundamentals$symbol <- ticker
+  # Remove the long description field
+  if (include_description == FALSE) {
+    fundamentals <- fundamentals[, !names(fundamentals) %in% c("description", "instrument")]
+  }
 
-    # Remove the long description field
-    if (include_description == FALSE) {
-        fundamentals <- fundamentals[, !names(fundamentals) %in% c("description", "instrument")]
-      }
+  # Format
+  fundamentals <- fundamentals %>%
+    dplyr::mutate_at(c("open", "high", "low", "volume", "average_volume_2_weeks", "average_volume", "high_52_weeks",
+                       "dividend_yield", "float", "low_52_weeks", "market_cap", "pb_ratio", "pe_ratio",
+                       "shares_outstanding"),
+                     as.numeric)
 
-    # Format
-    fundamentals <- fundamentals %>%
-      dplyr::mutate_at(c("open", "high", "low", "volume", "average_volume_2_weeks", "average_volume", "high_52_weeks",
-                         "dividend_yield", "float", "low_52_weeks", "market_cap", "pb_ratio", "pe_ratio",
-                         "shares_outstanding"),
-                       as.numeric)
-
-    return(fundamentals)
+  return(fundamentals)
 }
